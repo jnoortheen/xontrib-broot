e05eab6ff4e02523abbb312a39b487b6f8c525ed
Noortheen Raja <jnoortheen@gmail.com>
2021-06-04 22:32:50 +0530

    refactor: use colors if ran interactively
    fixes #1

--- xontrib/broot.py
+++ xontrib/broot.py
@@ -8,13 +8,15 @@ def _br(args, stdin, stdout, stderr):
     cmd_file = tempfile.NamedTemporaryFile(delete=False)
 
     try:
+        cmds = ("broot", "--outcmd", cmd_file.name) + tuple(args)
+        if builtins.__xonsh__.env.get("XONSH_INTERACTIVE"):
+            cmds += ("--color", "yes")
         subprocess.call(
-            ["broot", "--outcmd", cmd_file.name] + list(args),
+             cmds,
             stdin=stdin, stderr=stderr, stdout=stdout,
         )
         builtins.evalx(cmd_file.read().decode())
     finally:
         os.remove(cmd_file.name)
-
 
 builtins.aliases["br"] = _br
